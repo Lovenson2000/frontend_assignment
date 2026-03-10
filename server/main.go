1c63efa71b2e1ae44c04521dfb1cf0fe144ac337
Lovenson <beaucicotlovenson@gmail.com>
2026-03-10 17:52:28 +0800

    feat: Added basic health endpoint

--- server/main.go
+++ server/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v3"
+)
 
 func main() {
 	fmt.Print("Hello world")
+
+	app := fiber.New(fiber.Config{
+		BodyLimit: 100 * 1024 * 1024,
+	})
+
+	app.Get("/health", func(c fiber.Ctx) error {
+		return c.SendString("Server is Healthy and Running")
+	})
+
+	app.Listen(":8000")
 }
