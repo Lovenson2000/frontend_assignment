68869f33cad47d148db1f42bbd2244035a29d488
Lovenson <beaucicotlovenson@gmail.com>
2026-03-14 21:18:54 +0800

    feat: Save Cart items in local storage

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -3,6 +3,8 @@ import { cartReducer } from "../features/cart/cartSlice"
 import { menuApi } from "../features/menu/menuApi"
 import { menuReducer } from "../features/menu/menuSlice"
 
+const CART_STORAGE_KEY = "cart-items"
+
 // THIS WAY, MORE REDUCERS CAN BE ADDED IN THE FUTURE WITHOUT CHANGING THIS FILE
 const rootReducer = combineReducers({
   cart: cartReducer,
@@ -12,15 +14,50 @@ const rootReducer = combineReducers({
 
 export type RootState = ReturnType<typeof rootReducer>
 
+const loadCartPreloadedState = (): Partial<RootState> | undefined => {
+  if (typeof window === "undefined") {
+    return undefined
+  }
+
+  const storedCartItems = window.localStorage.getItem(CART_STORAGE_KEY)
+
+  if (!storedCartItems) {
+    return undefined
+  }
+
+  try {
+    const items = JSON.parse(storedCartItems)
+
+    if (!Array.isArray(items)) {
+      return undefined
+    }
+
+    return {
+      cart: {
+        items,
+      },
+    }
+  } catch {
+    return undefined
+  }
+}
+
 export const makeStore = (preloadedState?: Partial<RootState>) =>
   configureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) =>
+    middleware: getDefaultMiddleware =>
       getDefaultMiddleware().concat(menuApi.middleware),
-    preloadedState,
+    preloadedState: preloadedState ?? loadCartPreloadedState(),
   })
 
 export type AppStore = ReturnType<typeof makeStore>
 export type AppDispatch = AppStore["dispatch"]
 
 export const store = makeStore()
+
+if (typeof window !== "undefined") {
+  store.subscribe(() => {
+    const { items } = store.getState().cart
+    window.localStorage.setItem(CART_STORAGE_KEY, JSON.stringify(items))
+  })
+}
